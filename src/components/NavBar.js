538b176cfc5701039b040e30394cc692431a31b3
Ida <so.kida@hotmail.com>
2021-06-21 14:56:39 -0400

    animation for burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import {useTransition, animated} from 'react-spring'
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,6 +15,13 @@ export default function NavBar({active, setActive, goToHome, goToAbout, goToServ
 
     const [toggle, setToggle] = useState(false);
     const [windowWidt, setWindowWidth] = useState(window.innerWidth)
+
+    const transition = useTransition(toggle,(p)=>p,{
+        from: {opacity: 0,  transform: 'translateY(-30px)'  },
+        enter: {opacity: 1, transform: 'translateY(0px)' },
+        leave: { opacity: 0, transform: 'translateY(-60px)' },
+        delay:1000,
+    });
 
 
 
@@ -41,28 +49,40 @@ useEffect(() => {
 
 
     return (
+      
         <nav>
 
         <div className='burger-menu' >
-        {!toggle &&  <FontAwesomeIcon icon={faBars} onClick={()=>setToggle(!toggle)}/>}
-            
-            {toggle && <FontAwesomeIcon icon={faTimes} onClick={()=>setToggle(!toggle)}/>}
-            
+        {toggle ?  <FontAwesomeIcon icon={faTimes} onClick={()=>setToggle(!toggle)} />
+        :<FontAwesomeIcon icon={faBars} onClick={()=>setToggle(!toggle)}/> 
+       
+        }
+           
         </div>
 
-                {toggle  &&
-                                    <ul style={toggle===true ? {animationFillMode:'forwards'} : {animationFillMode:'both'}}>
-                                        <li onClick={goToHome}><Link>Home</Link></li>
-                                        <li onClick={goToAbout}><Link>About</Link></li>
-                                        <li onClick={goToServices}><Link>Services</Link></li>
-                                        <li onClick={goToGallery}><Link >Gallery</Link></li>
-                                        <li onClick={goToContact}><Link>Contact</Link></li>
-                                    </ul>
-                }
-           
-            
-        
+   
+       {/* {toggle ? <ul >
+                    <li onClick={goToHome}><Link>Home</Link></li>
+                    <li onClick={goToAbout}><Link>About</Link></li>
+                    <li onClick={goToServices}><Link>Services</Link></li>
+                    <li onClick={goToGallery}><Link >Gallery</Link></li>
+                    <li onClick={goToContact}><Link>Contact</Link></li>
+                </ul>
+                : ''} */}
 
+        
+         {transition.map(({ item, props, key })=>{
+             return item && <animated.ul style={props}>
+                    <li onClick={goToHome}><Link>Home</Link></li>
+                    <li onClick={goToAbout}><Link>About</Link></li>
+                    <li onClick={goToServices}><Link>Services</Link></li>
+                    <li onClick={goToGallery}><Link >Gallery</Link></li>
+                    <li onClick={goToContact}><Link>Contact</Link></li>
+                </animated.ul>
+         })}                   
+  
         </nav>
+       
+
     )
 }
